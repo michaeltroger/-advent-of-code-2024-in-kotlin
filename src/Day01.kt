4ca1840d030f430cfd4ea6e8f4b9a27754db4e17
Michael Troger <github@troger.app>
2024-12-01 14:11:07 +0100

    Solve day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,51 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputAsNumbers = input.map {
+            it.split("   ")
+                .map { it.toInt() }
+        }
+        val firstList = inputAsNumbers.map {
+            it[0]
+        }.sorted()
+        val secondList = inputAsNumbers.map {
+            it[1]
+        }.sorted()
+
+        return (inputAsNumbers.indices).map {
+            firstList[it] to secondList[it]
+        }.sumOf {
+            abs(it.first - it.second)
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val inputAsNumbers = input.map {
+            it.split("   ")
+                .map { it.toInt() }
+        }
+        val firstList = inputAsNumbers.map {
+            it[0]
+        }
+        val secondList = inputAsNumbers.map {
+            it[1]
+        }.groupingBy { it }.eachCount()
+
+        return (inputAsNumbers.indices).sumOf {
+            val timesInSecondList = secondList[firstList[it]] ?: 0
+            firstList[it] * timesInSecondList
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
+    // day1
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
+
+    check(part1(testInput) == 11) { "part1 check failed" }
     part1(input).println()
+
+    // day2
+    check(part2(testInput) == 31) { "part2 check failed" }
     part2(input).println()
 }
